f1fb867a2cb4ca6d19e8fa33cbf22bec48c615b9
greendavegreen <ec2-user@ip-10-165-68-74.ec2.internal>
2015-02-12 15:38:10 +0000

    Article 41

--- genSitemap.py
+++ genSitemap.py
@@ -30,7 +30,7 @@ class SiteMap:
 
         self.mainPage()
 
-        items = range(7, 37) + [37] + [38] + [39]
+        items = range(7, 37) + [37] + [38] + [39] + [41]
 
         for i in items:
             self.aURL("""http://elementascience.org/article/info:doi/10.12952/journal.elementa.""" +
